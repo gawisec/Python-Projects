ea5fb5fa7de0483c42de4a5e78156f63aca47b32
agamestory <gawisec@pm.me>
2018-08-03 02:17:31 +0800

    Added new files.

--- ex13.py
+++ ex13.py
@@ -1,6 +1,7 @@
 from sys import argv
 
-script, first, second, third, hehe = argv
+script, first, second, third, hehe = argv #Unpacker
+#Takes whatever the argument is, stores and uses it later for use.
 
 print "The script is called:", script
 print "Your first variable is:", first
